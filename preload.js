7dd5fd62d604ca695ba1b66a3c2ead55ccb772d9
David Knox <dakdave26@gmail.com>
2020-11-26 11:57:35 -0500

    Moved old code to oldspec folder.

--- preload.js
+++
@@ -1,8 +0,0 @@
-window.ipcRenderer = require('electron').ipcRenderer;
-// const dialog = require('electron').remote.dialog;
-
-const remote = window.require('electron').remote;
-remote.dialog.showOpenDialog(remote.getCurrentWindow(), {properties:["openDirectory"]});
-
-// window.electron = {};
-// window.electron.dialog = dialog;
